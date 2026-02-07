81b3731c6c6951adb05812e589c2156c537c100b
Wilfredo Aybar <aybar7w@gmail.com>
2026-02-07 08:41:25 -0800

    Added .gitignore, fixed "salt" syntax in init, updated code format to fit within 80 char limit

--- proj1_starter/password_manager.py
+++ proj1_starter/password_manager.py
@@ -26,27 +26,44 @@ class Keychain:
         ########### END CODE HERE ###########
     ):
         """
-        Initializes the keychain using the provided information. Note that external users should
-        likely never invoke the constructor directly and instead use either Keychain.new or
-        Keychain.load.
+        Initializes the keychain using the provided information. Note that 
+        external users should likely never invoke the constructor directly and 
+        instead use either Keychain.new or Keychain.load.
 
         Args:
-            You may design the constructor with any additional arguments you would like.
+            You may design the constructor with any additional arguments you 
+            would like.
         Returns:
             None
         """
         ########## START CODE HERE ##########
         self.data = {
-            salt: get_random_bytes(16),
+            "salt": get_random_bytes(16),
             # Store member variables that you intend to be public here
-            # (i.e. information that will not compromise security if an adversary sees).
+            # (i.e. information that will not compromise security if an 
+            # adversary sees).
             # This data should be dumped by the Keychain.dump function.
-            # You should store the key-value store (KVS) in the "kvs" item in this dictionary.
+            # You should store the key-value store (KVS) in the "kvs" item in 
+            # this dictionary.
             "kvs": None,
         }
-        keychain_password_holder = PBKDF2(keychain_password, self.data["salt"], 32, count=PBKDF2_ITERATIONS, hmac_hash_module=SHA256)
-        dom_key = HMAC.new(keychain_password_holder, b"domain",digestmod=SHA256).digest()
-        enc_key = HMAC.new(keychain_password_holder, b"encryption",digestmod=SHA256).digest()
+        keychain_password_holder = PBKDF2(
+            keychain_password,
+            self.data["salt"],
+            32,
+            count=PBKDF2_ITERATIONS,
+            hmac_hash_module=SHA256
+        )
+        dom_key = HMAC.new(
+            keychain_password_holder,
+            b"domain",
+            digestmod=SHA256
+        ).digest()
+        enc_key = HMAC.new(
+            keychain_password_holder,
+            b"encryption",
+            digestmod=SHA256
+        ).digest()
             
         self.secrets = {
             "dom_key": dom_key,
@@ -77,62 +94,79 @@ class Keychain:
 
     @staticmethod
     def load(
-        keychain_password: str, repr: str, trusted_data_check: Optional[bytes] = None
+        keychain_password: str, repr: str, 
+        trusted_data_check: Optional[bytes] = None
     ) -> "Keychain":
         """
         Creates a new keychain from an existing key-value store.
 
-        Loads the keychain state from the provided representation (repr). You can assume that
-        the representation passed to load is well-formed (i.e., it will be a valid JSON object)
-        and was generated from the Keychain.dump function.
+        Loads the keychain state from the provided representation (repr).
+        You can assume that the representation passed to load is
+        well-formed (i.e., it will be a valid JSON object) and was
+        generated from the Keychain.dump function.
 
-        Use the provided `json_str_to_dict` function to convert a JSON string into a nested dictionary.
+        Use the provided `json_str_to_dict` function to convert a JSON
+        string into a nested dictionary.
 
         Args:
             keychain_password: the password to unlock the keychain
-            repr: a JSON-encoded serialization of the contents of the key-value store (string)
-            trusted_data_check: an optional SHA-256 checksum of the KVS (bytes or None)
+            repr: a JSON-encoded serialization of the KVS (string)
+            trusted_data_check: an optional SHA-256 checksum of the
+                KVS (bytes or None)
         Returns:
             A Keychain instance containing the data from repr
         Throws:
-            ValueError: if the checksum is provided in trusted_data_check and the checksum check fails
-            ValueError: if the provided keychain password is not correct for the repr (hint: this is
-                thrown for you by HMAC.verify)
+            ValueError: if the checksum is provided in
+                trusted_data_check and the checksum check fails
+            ValueError: if the provided keychain password is not
+                correct for the repr (hint: this is thrown for you by
+                HMAC.verify)
         """
         ########## START CODE HERE ##########
-        raise NotImplementedError("Delete this line once you've implemented Keychain.load")
+        raise NotImplementedError(
+            "Delete this line once you've implemented Keychain.load"
+        )
         ########### END CODE HERE ###########
 
     def dump(self) -> Tuple[str, bytes]:
         """
-        Returns a JSON serialization and a checksum of the contents of the keychain that can be
-        loaded back using the Keychain.load function.
+        Returns a JSON serialization and a checksum of the
+        contents of the keychain that can be loaded back using the
+        Keychain.load function.
 
-        For testing purposes, please ensure that the JSON string you return contains the key
-        'kvs' with your KVS dict as its value. The KVS should have one key per domain.
+        For testing purposes, please ensure that the JSON string you
+        return contains the key 'kvs' with your KVS dict as its value.
+        The KVS should have one key per domain.
 
-        Use the provided `dict_to_json_str` function to convert a nested dictionary into
-        its JSON representation.
+        Use the provided `dict_to_json_str` function to convert a
+        nested dictionary into its JSON representation.
 
         Returns:
-            A tuple consisting of (1) the JSON serialization of the contents, and (2) the SHA256
-            checksum of the JSON serialization
+            A tuple consisting of (1) the JSON serialization of the
+            contents, and (2) the SHA256 checksum of the JSON
+            serialization
         """
         ########## START CODE HERE ##########
-        raise NotImplementedError("Delete this line once you've implemented Keychain.dump")
+        raise NotImplementedError(
+            "Delete this line once you've implemented Keychain.dump"
+        )
         ########### END CODE HERE ###########
 
     def get(self, domain: str) -> Optional[str]:
         """
-        Fetches the password corresponding to a given domain from the key-value store.
+        Fetches the password corresponding to a given domain from
+        the key-value store.
 
         Args:
             domain: the domain for which the password is requested
         Returns:
-            The password for the domain if it exists in the KVS, or None if it does not exist
+            The password for the domain if it exists in the KVS,
+            or None if it does not exist
         """
         ########## START CODE HERE ##########
-        raise NotImplementedError("Delete this line once you've implemented Keychain.get")
+        raise NotImplementedError(
+            "Delete this line once you've implemented Keychain.get"
+        )
         ########### END CODE HERE ###########
 
     def set(self, domain: str, password: str):
@@ -142,23 +176,31 @@ class Keychain:
         If it is not, a new entry in the password manager is created.
 
         Args:
-            domain: the domain for the provided password. This domain may already exist in the KVS
+            domain: the domain for the provided password. This
+                domain may already exist in the KVS
             password: the password for the provided domain
         """
         ########## START CODE HERE ##########
-        raise NotImplementedError("Delete this line once you've implemented Keychain.set")
+        raise NotImplementedError(
+            "Delete this line once you've implemented Keychain.set"
+        )
         ########### END CODE HERE ###########
 
     def remove(self, domain: str) -> bool:
         """
-        Removes the domain-password pair for the provided domain from the password manager.
-        If the domain does not exist in the password manager, this method deos nothing.
+        Removes the domain-password pair for the provided domain
+        from the password manager. If the domain does not exist in the
+        password manager, this method deos nothing.
 
         Args:
-            domain: the domain which should be removed from the KVS, along with its password
+            domain: the domain which should be removed from the
+                KVS, along with its password
         Returns:
-            True if the domain existed in the KVS and was removed, False otherwise
+            True if the domain existed in the KVS and was removed,
+            False otherwise
         """
         ########## START CODE HERE ##########
-        raise NotImplementedError("Delete this line once you've implemented Keychain.remove")
+        raise NotImplementedError(
+            "Delete this line once you've implemented Keychain.remove"
+        )
         ########### END CODE HERE ###########
